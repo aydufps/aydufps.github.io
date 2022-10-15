c9560e03c7f8e1a23c77ac14334e51f6a313b41e
Jeferson Hernandez <j.hernandez@nelumboconsultores.com>
2022-10-15 11:34:05 -0500

    feat: add firebase

---
+++ infrastructure/firebase.js
@@ -0,0 +1,10 @@
+const firebaseDevelopConfig = {
+  apiKey: 'AIzaSyD4ji--NwWcc67xTfhtEvheCV_Nn-ADbvs',
+  authDomain: 'ufpsfincas.firebaseapp.com',
+  projectId: 'ufpsfincas',
+  storageBucket: 'ufpsfincas.appspot.com',
+  messagingSenderId: '746517620446',
+  appId: '1:746517620446:web:b6cec2ddbc153d674c844f',
+  measurementId: 'G-WBBG2FQXES',
+};
+const database = firebase.initializeApp(firebaseDevelopConfig);
